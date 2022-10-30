8118a2b2f9a3dfe2cd6843ba41b5d724446bdf5e
tomtom <test.teck.web231564@gmail.com>
2022-10-30 06:37:04 +0000

    top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'toppages/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'tasks#index'
+  root to: 'toppages#index'
+  #toppageを最初の入り口にするためtasks#indexをコメントアウト（課題用）
+  #root to: 'tasks#index'
 
   resources :tasks
   
